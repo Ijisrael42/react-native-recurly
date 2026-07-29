680573db289077d4aba21673c9450fe6ed1963b2
Judah Oluwaseeki <isosafeapp@gmail.com>
2026-07-29 10:25:24 +0200

    feat: project setup, nativewinnd, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
       <Link href="/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
-    </View>
+    </SafeAreaView>
   );
 }
