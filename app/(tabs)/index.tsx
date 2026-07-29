46b25d18695e1f107a99bde820ffc7fafcb6a146
Judah Oluwaseeki <isosafeapp@gmail.com>
2026-07-29 11:20:23 +0200

    feat: implemented fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,11 +7,9 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/sign-in" className="mt-4 rounded bg-primary text-white p-4">Sign In</Link>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-sans-bold text-primary">Home</Text>
+      <Link href="/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Sign In</Link>
     </SafeAreaView>
   );
 }
