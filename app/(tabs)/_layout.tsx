db0ee5b525d24bd88094b0a14d329915b5557175
Judah Oluwaseeki <isosafeapp@gmail.com>
2026-08-02 18:04:59 +0200

    feat: implement the subscriptions screen and the subscription create modal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
+import { SubscriptionsProvider } from "@/context/SubscriptionContext";
 import "@/global.css";
 import clsx from "clsx";
 import { Tabs } from "expo-router";
@@ -19,38 +20,42 @@ export default function RootLayout() {
   };
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          position: "absolute",
-          bottom: Math.max(insets.bottom, tabBar.horizontalInset),
-          height: tabBar.height,
-          marginHorizontal: tabBar.horizontalInset,
-          borderRadius: tabBar.radius,
-          backgroundColor: colors.primary,
-          borderTopWidth: 0,
-          elevation: 0
-        },
-        tabBarItemStyle: {
-          paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6
-        },
-        tabBarIconStyle: {
-          width: tabBar.iconFrame,
-          height: tabBar.iconFrame,
-          alignItems: "center",
-        }
-      }}>
-      {tabs.map((tab) => (
-        <Tabs.Screen
-          key={tab.name}
-          name={tab.name}
-          options={{
-            title: tab.title,
-            tabBarIcon: ({ focused }) =>
-              (<TabIcon focused={focused} icon={tab.icon} />)
-          }} />
-      ))}
-    </Tabs>);
+    <SubscriptionsProvider>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: "absolute",
+            bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+            height: tabBar.height,
+            marginHorizontal: tabBar.horizontalInset,
+            borderRadius: tabBar.radius,
+            backgroundColor: colors.primary,
+            borderTopWidth: 0,
+            elevation: 0
+          },
+          tabBarItemStyle: {
+            paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6
+          },
+          tabBarIconStyle: {
+            width: tabBar.iconFrame,
+            height: tabBar.iconFrame,
+            alignItems: "center",
+          }
+        }}>
+        {tabs.map((tab) => (
+          <Tabs.Screen
+            key={tab.name}
+            name={tab.name}
+            options={{
+              title: tab.title,
+              tabBarIcon: ({ focused }) =>
+                (<TabIcon focused={focused} icon={tab.icon} />)
+            }} />
+        ))}
+      </Tabs>
+    </SubscriptionsProvider>
+  );
 }
+
